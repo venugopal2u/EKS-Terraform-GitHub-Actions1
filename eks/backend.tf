29e707e1cb38b5caec26ea54a4b31b3244f8fb79
venugopal2u <57522384+venugopal2u@users.noreply.github.com>
2024-10-12 12:36:08 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket-ve"
+    bucket         = "my-ews-baket1-ve"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
